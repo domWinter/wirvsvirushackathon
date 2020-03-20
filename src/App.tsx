a3e138e3a7bc3c4e5897748d36f0f2f3c0ca8e5e
Kevin Kappelmann <accounts@kappelmann.me>
2020-03-20 22:54:21 +0100

    add i18 library

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-//import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
+import { FormattedMessage } from "react-intl";
+
 
 type AppProps = {
   className?: string
 }
 
 const App = ({ className } : AppProps) => (
-  <div className={className}>
-    Hello world
-  </div>
+  <FormattedMessage
+    id="app.greeting"
+    description="Greeting to welcome the user to the app"
+    defaultMessage="Hello, {name}!"
+    values={{
+      name: 'Alex',
+    }}>
+    {(txt) =>
+      <p className={className}>{txt}</p>
+    }
+  </FormattedMessage>
 );
 
 
