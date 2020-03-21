5e321c41075e33fb0d12feb003732f95072ba58c
Dominik Winter <dominik.winter@tum.de>
2020-03-21 02:03:41 +0100

    database connection

--- api/api.py
+++ api/api.py
@@ -1,32 +1,18 @@
+import psycopg2
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
 
+PASSWORD = os.getenv('PASSWORD') 
 
-mockHospitals = [
+try:
+    conn = psycopg2.connect(database = "wirvsvirushackathon", user = "app@postgres-wirvsvirushackathon", password = PASSWORD , host = "postgres-wirvsvirushackathon.postgres.database.azure.com", port = "5432")
+except:
+    print("I am unable to connect to the database") 
 
-    { 'id': 0, 'name':  "München Klinik Neuperlach",
-      'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81373", 'street' : "Oskar-Maria-Graf-Ring", 'streetNumber': "51"},
-      'phoneNumber' : '08967940',
-      'website' : 'https://www.muenchen-klinik.de/krankenhaus/neuperlach/',
-      'location': {'latitude': 48.094510, 'longitude' : 11.656720},
-      'beds': { 'iculc': 500, 'icuhc': 40, 'ecmo': 20}},
+cur = conn.cursor()
 
-    { 'id': 1, 'name':  "München Klinik Bogenhausen",
-      'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81925", 'street' : "Englschalkinger Str.", 'streetNumber': "77"},
-      'phoneNumber' : '08992700',
-      'website' : 'https://www.muenchen-klinik.de/krankenhaus/bogenhausen/',
-      'location': {'latitude': 48.155404, 'longitude': 11.624846},
-      'beds': { 'iculc': 600, 'icuhc': 30, 'ecmo': 10}},
-
-
-    { 'id': 2, 'name':  "München Klinik Schwabing",
-      'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "80804", 'street' : "Kölner Pl.", 'streetNumber': "1"},
-      'phoneNumber' : '08930680',
-      'website' : 'https://www.muenchen-klinik.de/krankenhaus/schwabing/',
-      'location': {'latitude': 48.171982, 'longitude' : 11.578332},
-      'beds': { 'iculc': 300, 'icuhc': 20, 'ecmo': 5}}
-]
 
 @app.route('/add/hospital', methods=['POST'])
 def addHospital():
@@ -34,15 +20,39 @@ def addHospital():
 
 @app.route('/hospitals', methods=['GET'])
 def getHospitals():
-    return {'data': mockHospitals}, 200
+    query =  "SELECT * FROM hospitals;"
+    cur.execute(query)
+    hospitalsSQL = cur.fetchall()
+
+    hospitals = []
+
+    try:
+        for row in hospitalsSQL:
+            hospitals.append({'id' : int(row[0]), 'name': str(row[1]),
+                              'address' : {'state' : str(row[2]), 'city' : str(row[3]), 'postcode': str(row[4]), 'street' : str(row[5]), 'streetNumber': str(row[6])},
+                              'phoneNumber' : str(row[7]),
+                              'website' : str(row[8]),
+                              'location' : { 'latitude' : str(row[9]), 'longitude' : str(row[10]) },
+                              'beds' : { 'iculc' : int(row[11]), 'icuhc' : int(row[12]), 'ecmo' : int(row[13])}
+                            }) 
+    except:
+        return {{}}, 500
+
+    return {"data" : hospitals}, 200
 
 @app.route('/hospital', methods=['GET'])
 def getHospitalByID():
-    requestHospitalID = int(request.args.get('id'))
-    hospital = {}
+    requestHospitalID = request.args.get('id')
+    query =  "SELECT * FROM hospitals WHERE id = %s;"
+    cur.execute(query, requestHospitalID)
+    hospitalSQL = cur.fetchall()[0]
 
-    for hospital in mockHospitals:
-        if hospital['id'] == requestHospitalID:
-            return {'data': hospital}, 200
+    hospital = {'id' : int(hospitalSQL[0]), 'name': str(hospitalSQL[1]), 
+                'address' : {'state' : str(hospitalSQL[2]), 'city' : str(hospitalSQL[3]), 'postcode': str(hospitalSQL[4]), 'street' : str(hospitalSQL[5]), 'streetNumber': str(hospitalSQL[6])},
+                'phoneNumber' : str(hospitalSQL[7]),
+                'website' : str(hospitalSQL[8]),
+                'location' : { 'latitude' : str(hospitalSQL[9]), 'longitude' : str(hospitalSQL[10]) },
+                'beds' : { 'iculc' : int(hospitalSQL[11]), 'icuhc' : int(hospitalSQL[12]), 'ecmo' : int(hospitalSQL[13])}
+                }
 
-    return {'hospital': {}}, 404
+    return {"data": hospital}, 200
