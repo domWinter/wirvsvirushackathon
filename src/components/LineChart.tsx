eca72512c9939953558a38d352b553c7f4c41736
Kevin Kappelmann <accounts@kappelmann.me>
2020-03-21 19:06:35 +0100

    update line chart dots

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ResponsiveLine } from 'nivo';
+import { useIntl, defineMessages } from "react-intl";
 
 type LineChartProps = {
   data: object,
@@ -12,13 +13,38 @@ type LineChartProps = {
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
-const LineChart = ({ data, height, width } : LineChartProps) => (
+const LineChart = ({ data, height, width } : LineChartProps) => {
+  const intl = useIntl();
+
+  const messages = defineMessages({
+    date: { id: 'date', defaultMessage: 'Datum' },
+  });
+
+  return (
     <div style={{height, width}}>
       <ResponsiveLine
           data={data}
           margin={{ top: 50, right: 50, bottom: 50, left: 100 }}
+          axisBottom={{
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            legend: intl.formatMessage(messages.date),
+            legendOffset: 36,
+            legendPosition: 'center'
+        }}
+        axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            legend: 'count',
+            legendOffset: -40,
+            legendPosition: 'center'
+        }}
+        dotSize={12}
       />
     </div>
-)
+  );
+}
 
 export default LineChart;
