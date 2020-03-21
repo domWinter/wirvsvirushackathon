0bfa57fac226059817416ff14662a70cdbb7fef6
Dominik Winter <dominik.winter@tum.de>
2020-03-21 15:54:56 +0100

    added api unittests

--- api/tests.py
+++ api/tests.py
@@ -1,47 +1,51 @@
 import requests
+import unittest
 
 
-def testHospitalAdd():
-    hospital1 = { 'name': "München Klinik Bogenhausen",
-                'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81925", 'street' : "Englschalkinger Str.", 'streetNumber': "77"},
-                'phoneNumber' : '08992700',
-                'website' : 'https://www.muenchen-klinik.de/krankenhaus/bogenhausen/',
-               }
-
-    hospital2 = { 'name':  "München Klinik Neuperlach",
-                 'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81373", 'street' : "Oskar-Maria-Graf-Ring", 'streetNumber': "51"},
-                 'phoneNumber' : '08967940',
-                 'website' : 'https://www.muenchen-klinik.de/krankenhaus/neuperlach/',
-               }
-
-    hospital3 = { 'name':  "München Klinik Schwabing",
-                 'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "80804", 'street' : "Kölner Pl.", 'streetNumber': "1"},
-                 'phoneNumber' : '08930680',
-                 'website' : 'https://www.muenchen-klinik.de/krankenhaus/schwabing/'
+class TestApiRoutes(unittest.TestCase):
+    """
+    def testHospitalAdd(self):
+        hospital1 = { 'name': "München Klinik Bogenhausen",
+                    'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81925", 'street' : "Englschalkinger Str.", 'streetNumber': "77"},
+                    'phoneNumber' : '08992700',
+                    'website' : 'https://www.muenchen-klinik.de/krankenhaus/bogenhausen/',
                 }
 
-    r = requests.post("http://127.0.0.1:5000/hospital/add", json={'data': hospital1})
-    print(r.status_code, r.reason)
+        hospital2 = { 'name':  "München Klinik Neuperlach",
+                    'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "81373", 'street' : "Oskar-Maria-Graf-Ring", 'streetNumber': "51"},
+                    'phoneNumber' : '08967940',
+                    'website' : 'https://www.muenchen-klinik.de/krankenhaus/neuperlach/',
+                }
 
-def testHospitals():
-    r = requests.get("http://127.0.0.1:5000/hospitals")
-    print(r.status_code, r.reason)
-    print(r.text)
+        hospital3 = { 'name':  "München Klinik Schwabing",
+                    'address': {'state' : "Bayern" , 'city' : "München", 'postcode': "80804", 'street' : "Kölner Pl.", 'streetNumber': "1"},
+                    'phoneNumber' : '08930680',
+                    'website' : 'https://www.muenchen-klinik.de/krankenhaus/schwabing/'
+                    }
 
-def testHospital(id):
-    r = requests.get("http://127.0.0.1:5000/hospital" + "?id=" + str(id))
-    print(r.status_code, r.reason)
-    print(r.text)
+        r = requests.post("http://127.0.0.1:5000/hospital/add", json={'data': hospital1})
+        print(r.status_code, r.reason)
+    """
 
-def testBedavailability(id):
-    r = requests.get("http://127.0.0.1:5000/bedavailability" + "?id=" + str(id))
-    print(r.status_code, r.reason)
-    print(r.text)
-
-def testLatestBedavailability(id):
-    r = requests.get("http://127.0.0.1:5000/bedavailability/latest" + "?id=" + str(id))
-    print(r.status_code, r.reason)
-    print(r.text)
+    def testHospitals(self):
+        r = requests.get("http://127.0.0.1:5000/hospitals")
+        self.assertEqual(str(r.status_code), '200')
 
 
-testLatestBedavailability(1)
+    def testHospital(self):
+        id = 1
+        r = requests.get("http://127.0.0.1:5000/hospital" + "?id=" + str(id))
+        self.assertEqual(str(r.status_code), '200')
+
+    def testBedavailability(self):
+        id = 1
+        r = requests.get("http://127.0.0.1:5000/bedavailability" + "?id=" + str(id))
+        self.assertEqual(str(r.status_code), '200')
+
+    def testLatestBedavailability(self):
+        id = 1
+        r = requests.get("http://127.0.0.1:5000/bedavailability/latest" + "?id=" + str(id))
+        self.assertEqual(str(r.status_code), '200')
+
+if __name__ == '__main__':
+    unittest.main()
