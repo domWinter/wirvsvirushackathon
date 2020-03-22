dea423b39cb92af405dc948f73fa303179f0939c
Dominik Winter <dominik.winter@tum.de>
2020-03-22 01:13:34 +0100

    refactored main view

--- src/components/MainRoute.tsx
+++ src/components/MainRoute.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-
+import {Container, Row, Col, Card} from 'react-bootstrap';
 import HospitalsRoute from './HospitalsRoute';
 import MapRoute from './MapRoute';
 
 export const MainRoute = () => {
   return (
     <>
-      <MapRoute />
-      <HospitalsRoute />
+      <Container>
+        <Row>
+          <Col>
+            <Card className="bg-dark text-white">
+              <Card.Header><h2>Krankenhäuser</h2></Card.Header>
+              <HospitalsRoute />
+            </Card>
+          </Col>
+        </Row> 
+        <Row>
+          <Col>
+            <Card className="bg-dark text-white">
+            <Card.Header><h2>Heatmap</h2></Card.Header>
+                <MapRoute />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
