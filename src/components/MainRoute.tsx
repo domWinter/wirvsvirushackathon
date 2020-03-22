1467f575454be860e0e2ac5ee593e2fcb61102a8
Kevin Kappelmann <accounts@kappelmann.me>
2020-03-22 11:17:25 +0100

    update layout

--- src/components/MainRoute.tsx
+++ src/components/MainRoute.tsx
@@ -11,6 +11,27 @@ export const MainRoute = () => {
           <Col style={{ maxWidth: '100%', 'padding': 0 }}>
             <Card className="bg-dark text-white">
               <FormattedMessage
+                id="heatMap"
+                description="heatMap"
+                defaultMessage="Heat Map"
+              >
+                {(txt) => <Card.Header><h2>{txt}</h2></Card.Header>}
+              </FormattedMessage>
+              <MapRoute />
+              <FormattedMessage
+                id="heatMapEpxlanation"
+                description="Explanation for the heat map"
+                defaultMessage="Use the layer button on the right to visualise the bed capacities for different categories. You can change the date using the slider. Click on a marker to get more information about the chosen hospital. ICULC=TODO, etc."
+              >
+                {(txt) => <p>{txt}</p>}
+              </FormattedMessage>
+            </Card>
+          </Col>
+        </Row>
+        <Row style={{margin: 0}}>
+          <Col style={{ maxWidth: '100%', 'padding': 0 }}>
+            <Card className="bg-dark text-white">
+              <FormattedMessage
                 id="hospitals"
                 description="hospitals"
                 defaultMessage="Hospitals"
@@ -21,27 +42,6 @@ export const MainRoute = () => {
             </Card>
           </Col>
         </Row> 
-        <Row style={{margin: 0}}>
-          <Col style={{ maxWidth: '100%', 'padding': 0 }}>
-            <Card className="bg-dark text-white">
-              <FormattedMessage
-                id="heatMap"
-                description="heatMap"
-                defaultMessage="Heat Map"
-              >
-                {(txt) => <Card.Header><h2>{txt}</h2></Card.Header>}
-              </FormattedMessage>
-              <FormattedMessage
-                id="heatMapEpxlanation"
-                description="Explanation for the heat map"
-                defaultMessage="Use the layer button on the right to visualise the bed capacities for different categories. You can change the date using the slider. Click on a marker to get more information about the chosen hospital. ICULC=TODO, etc."
-              >
-                {(txt) => <p>{txt}</p>}
-              </FormattedMessage>
-              <MapRoute />
-            </Card>
-          </Col>
-        </Row>
     </>
   );
 };
