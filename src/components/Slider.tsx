ae631dfe8bda8d21fcabd25d38330b43679fe982
Kevin Kappelmann <accounts@kappelmann.me>
2020-03-22 14:26:42 +0100

    undo slider throttle

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,18 +1,19 @@
-import React, {useState,useRef} from 'react';
+import React, {useState} from 'react';
+// import React, {useState,useRef} from 'react';
 import InputRange from 'react-input-range';
 import 'react-input-range/lib/css/index.css';
-import _ from "lodash";
+// import _ from "lodash";
 
-const THROTTLE = 800;
+// const THROTTLE = 800;
 
 export const Slider = ({max,min,onChange,formatLabel}) => {
   const [value,setValue] = useState(max);
-  const throttledOnChange = useRef(_.throttle(value => onChange(value), THROTTLE)).current;
+  // const throttledOnChange = useRef(_.throttle(value => onChange(value), THROTTLE)).current;
 
-  const handleChange = (value) => {
-    setValue(value);
-    throttledOnChange(value);
-  };
+  // const handleChange = (value) => {
+    // setValue(value);
+    // throttledOnChange(value);
+  // };
 
   return (
     <InputRange
@@ -20,7 +21,8 @@ export const Slider = ({max,min,onChange,formatLabel}) => {
       maxValue={max}
       minValue={min}
       value={value}
-      onChange={(value) => { setValue(value); handleChange(value) }}
+      onChange={setValue}
+      onChangeComplete={onChange}
     />
   );
 };
