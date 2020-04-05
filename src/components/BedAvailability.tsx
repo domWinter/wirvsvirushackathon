7e1267e930834383fcd509ce6ec9d7d56302d710
Stephan Egli <stephan.egli93@gmail.com>
2020-04-05 09:09:01 +0200

    Added prototype changes to add new Field in popup infos without the backend changes yet

--- src/components/BedAvailability.tsx
+++ src/components/BedAvailability.tsx
@@ -6,6 +6,7 @@ import { FormattedMessage, FormattedDate, FormattedTime } from "react-intl";
 import { compVariant } from '../utils/utils';
 
 const BedAvailability = ({
+  nurses,
   iculc,
   iculcMax,
   icuhc,
@@ -18,6 +19,12 @@ const BedAvailability = ({
     <Table style={ { height: 145 } } striped bordered hover variant="dark">
       <thead>
         <tr>
+        <FormattedMessage
+            id="nurses"
+            defaultMessage="Professional Nurses"
+          >
+            {(txt) => <th>{txt}</th>}
+          </FormattedMessage>
           <FormattedMessage
             id="totalCapacity"
             defaultMessage="Total Capacity"
--- src/types.tsx
+++ src/types.tsx
@@ -1,58 +1,59 @@
 export type Address = {
-    state: string,
-    city: string,
-    postcode: string,
-    street: string,
-    streetNumber: number
+  state: string,
+  city: string,
+  postcode: string,
+  street: string,
+  streetNumber: number
 };
 
 export type BedAvailability = {
-  id: number,
-  hospitalID: number,
-  iculc: number,
-  iculcMax: number,
-  icuhc: number,
-  icuhcMax: number,
-  ecmo: number,
-  ecmoMax: number,
-  timestamp: string
+id: number,
+hospitalID: number,
+nurses: number,
+iculc: number,
+iculcMax: number,
+icuhc: number,
+icuhcMax: number,
+ecmo: number,
+ecmoMax: number,
+timestamp: string
 }
 
 export type HeatMapEntry = Location & {
-  sumIntensity: number,
-  iculcIntensity: number,
-  icuhcIntensity: number,
-  ecmoIntensity: number
+sumIntensity: number,
+iculcIntensity: number,
+icuhcIntensity: number,
+ecmoIntensity: number
 }
 
 export type HeatMap = HeatMapEntry[];
 
 export type MapDataEntry =
-  Exclude<Hospital, "location" | "address"> & Location & Address & BedAvailability;
+Exclude<Hospital, "location" | "address"> & Location & Address & BedAvailability;
 
 export type MapData = MapDataEntry[];
 
 export type Marker = Location & Exclude<Hospital, "location" | "address"> & Exclude<BedAvailability, "id" | "hospitalID"> & Address
 
 export type Location = {
-    longitude: number,
-    latitude: number
+  longitude: number,
+  latitude: number
 };
 
 export type Hospital = {
-    id: number,
-    name: string,
-    address: Address,
-    phoneNumber: string,
-    website: string,
-    location: Location
+  id: number,
+  name: string,
+  address: Address,
+  phoneNumber: string,
+  website: string,
+  location: Location
 };
 
 export interface Repository {
-    addHospital(hospital : Partial<Hospital>) : Promise<{}>;
-    getBedAvailabilityLatest(id: number) : Promise<BedAvailability>;
-    getBedAvailability(id: number) : Promise<BedAvailability[]>;
-    getHospitals() : Promise<Hospital[]>;
-    getHospitalById(id: number) : Promise<Hospital>;
-    getMapData({date: number}) : Promise<MapData>;
+  addHospital(hospital : Partial<Hospital>) : Promise<{}>;
+  getBedAvailabilityLatest(id: number) : Promise<BedAvailability>;
+  getBedAvailability(id: number) : Promise<BedAvailability[]>;
+  getHospitals() : Promise<Hospital[]>;
+  getHospitalById(id: number) : Promise<Hospital>;
+  getMapData({date: number}) : Promise<MapData>;
 }
